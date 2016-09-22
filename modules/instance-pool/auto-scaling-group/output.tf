a2323c97cfaaeb1eccffe78709249df16cb8d326
Hazim <hazim@aurorasolutions.io>
2016-09-22 12:50:06 +0500

    remove policy and sns output

--- modules/instance-pool/auto-scaling-group/output.tf
+++ modules/instance-pool/auto-scaling-group/output.tf
@@ -7,12 +7,3 @@ output "asg_id" {
 output "asg_name" {
   value = "${coalesce(join(",",aws_autoscaling_group.asg.*.name),join(",",aws_autoscaling_group.asg_elb.*.name))}"
 }
-
-
-output "policy_arn" {
-  value = "${coalesce(join(",",aws_autoscaling_policy.asg_policy.*.arn),join(",",aws_autoscaling_policy.asg_policy_percent.*.arn))}"
-}
-
-output "sns_arn" {
-  value = "${aws_sns_topic.sns_asg.arn}"
-}
