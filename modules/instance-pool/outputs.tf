5987d5a848db44874a811cd9c5a40e6f2a8cc9c8
hazim1093 <hazim_malik@hotmail.com>
2017-06-07 16:15:43 +0500

    Remove Test outputs

--- modules/instance-pool/outputs.tf
+++ modules/instance-pool/outputs.tf
@@ -15,11 +15,3 @@ output "asg_id" {
 output "asg_name" {
   value = "${module.auto-scaling-group.asg_name}"
 }
-
-#TEST
-output "test-output-asg" {
-  value = "${module.auto-scaling-group.test-output-asg}"
-}
-output "test-output-asg-elb" {
-  value = "${module.auto-scaling-group.test-output-asg-elb}"
-}
