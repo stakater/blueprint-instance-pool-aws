bc581dff37d75c0bf1272e661580154bff13331c
hazim1093 <hazim_malik@hotmail.com>
2017-06-07 15:49:58 +0500

    Add test outputs

--- modules/instance-pool/auto-scaling-group/output.tf
+++ modules/instance-pool/auto-scaling-group/output.tf
@@ -7,3 +7,11 @@ output "asg_id" {
 output "asg_name" {
   value = "${coalesce(join(",",aws_autoscaling_group.asg.*.name),join(",",aws_autoscaling_group.asg_elb.*.name))}"
 }
+
+#TEST
+output "test-output-asg" {
+  value = "${(signum(length(compact(split(",",var.load_balancers)))) + 1) % 2}"
+}
+output "test-output-asg-elb" {
+  value = "${signum(length(compact(split(",",var.load_balancers))))}"
+}
